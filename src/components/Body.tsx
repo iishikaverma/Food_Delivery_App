28dd58e7045ae5675cd6df915a3ac30f8e19e281
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-06 00:01:42 +0530

    Add useEffect-based data fetching

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,14 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
 
     // Local State Variable - Super powerful variable
     const [listOfRestaurants, setListOfRestaurants] = useState(resList)
-
     // Normal JS Variable
     // let listOfRestaurants = [];
+
+    useEffect(()=>{
+        fetchData()
+    }, []);
+
+    const fetchData = async() => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6192679&lng=77.4265032&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+        console.log(json);
+    }
 
     return(
         <div className="body">
