b1bd28b97e8d2e6572ba399de2d95449b5e1f99c
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-13 23:53:40 +0530

    Add navigation to restaurant menu via clickable cards

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Restaurant } from "../utils/types";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Body = () => {
 
@@ -62,8 +63,7 @@ const Body = () => {
             <div className="res-container">
                 {
                     filteredRestaurant.map((restaurant) => (
-                        <RestaurantCard key={restaurant.info.id} 
-                        resData={restaurant}/>)
+                        <Link key={restaurant.info.id} to = {"/restaurants/" + restaurant.info.id}><RestaurantCard resData={restaurant}/></Link>)
                     )
                 }
             </div>
