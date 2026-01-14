65baae5b0b5950b6da2834e87a1170be47c5a907
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-14 14:46:51 +0530

    feat: add User and UserClass to About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>This is Namaste React web Series </h2>
+            <User name = {"Ishika Verma (function)"} location = {"Delhi"}/>
+            <UserClass name = {"Ishika Verma (class)"} location = {"Delhi"}/>
         </div>
     )
 }
