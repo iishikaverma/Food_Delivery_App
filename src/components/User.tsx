21e792516ba0e174dfc91c88e62a2db0b05af9fe
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-14 18:30:55 +0530

    update: improve User and UserClass components

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
 const User = (props:any) => {
+    const [count] = useState(0);
+
+    useEffect(() => {
+        //API Calls
+    }, [])
+
+    async function getUserInfo () {
+        const data = await fetch("");
+    }
+
     return <div className="user-card">
+        <h1>Count = {count}</h1>
         <h2>Name : {props.name}</h2>
-        <h3>Location: {props.locaion}</h3>
+        <h3>Location: {props.location}</h3>
         <h4>Contact: @ishika</h4>
     </div>
 }
--- src/components/UserClass.tsx
+++ src/components/UserClass.tsx
@@ -1,20 +1,45 @@
 import React from "react";
+import { UserProps, UserState } from "../utils/types";
 
-type UserProps = {
-  name: string;
-  location: string;
-};
-
-class UserClass extends React.Component<UserProps> {
+class UserClass extends React.Component<UserProps, UserState> {
     constructor(props:UserProps) {
         super(props);
-
-        console.log(props);
+        this.state = {
+            userInfo: {
+                name: "Dummy",
+                location: "Default",
+                avatar_url: "https://dummy-photo.com"
+            }
+        }
+        //console.log("child constructor");
     }
+
+    async componentDidMount() {
+        //console.log("Child component did mount")
+        const data = await fetch ("https://api.github.com/users/iishikaverma");
+        const json = await data.json();
+
+        this.setState({
+            userInfo: json
+        })
+
+        // console.log(json);
+    }
+
+    componentDidUpdate() {
+        console.log("Component did Update")
+    }
+
+    componentWillUnmount() {
+        console.log('Component Will Unmount');
+    }
+
     render() {
-        const {name,location} = this.props;
+        const {name,location, avatar_url} = this.state.userInfo;
+        // console.log("child rendered")
         return (
             <div className="user-card">
+                <img src={avatar_url} alt={"image"} />
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: @ishika</h4>
@@ -24,3 +49,24 @@ class UserClass extends React.Component<UserProps> {
 }
 
 export default UserClass;
+
+/* ***********************************************
+ *
+ *
+ * ----- Mounting CYCLE -----
+ *   Constructor (dummy)
+ *   Render (dummy)
+ *       <HTML Dummy></HTML>
+ *   Component Did Mount
+ *       <API Call>
+ *       <this.setState> - State variable is updated
+ *
+ * ----- UPDATE CYCLE -----
+ *       render(API data)
+ *       <HTML (new API data)>
+ *   Component Did Update
+ *   Component Will Unmount
+ *
+ *
+ * Life Cycle Diagram Website Reference: https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+ */
