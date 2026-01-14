ce0835a062b41d45e9640e3fef8c76ad86babd81
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-14 18:25:58 +0530

    convert About component to class-based component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,62 @@
-import User from "./User";
+import { Component } from "react";
+//import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React web Series </h2>
-            <User name = {"Ishika Verma (function)"} location = {"Delhi"}/>
-            <UserClass name = {"Ishika Verma (class)"} location = {"Delhi"}/>
-        </div>
-    )
+
+class About extends Component {
+
+    constructor(props:any) {
+        super(props);
+        this.state = {
+            
+        };
+        //console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        //console.log("Parent component did mount")
+    }
+
+    render() {
+        //console.log("Parent Rendered");
+        return(
+            <div>
+                <h1>About (Class Component)</h1>
+                <h2>This is Namaste React web Series </h2>
+                {/* <User name = {"Ishika Verma (function)"} location = {"Delhi"}/> */}
+                <UserClass name = {"Name1 (class)"} location = {"Delhi"}/>
+                {/* <UserClass name = {"Name2 (class)"} location = {"US"}/> */}
+            </div>
+        )
+    }
 }
+
+// * RENDER CYCLE OF CLASS BASED COMPONENTS WHEN THE CLASS HAS TWO CHLIDREN
+/* 
+  - Parent Constructor()              -- Render Phase
+  - Parent Render()
+
+    - First Child Constructor()
+    - First Child Render()
+                                      -- Render Phase
+    - Second Child Constructor()
+    - Second Child Render()
+
+     <DOM UPDATED - IN SINGLE BATCH> -> Optimizes the Performance of App  -- Commit Phase
+    - First Child ComponentDidMount()
+    - Second Child ComponentDidMount()
+
+  - Parent ComponentDidMount()=
+*/
+
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About</h1>
+//             <h2>This is Namaste React web Series </h2>
+//             <User name = {"Ishika Verma (function)"} location = {"Delhi"}/>
+//             <UserClass name = {"Ishika Verma (class)"} location = {"Delhi"}/>
+//         </div>
+//     )
+// }
 
 export default About;
